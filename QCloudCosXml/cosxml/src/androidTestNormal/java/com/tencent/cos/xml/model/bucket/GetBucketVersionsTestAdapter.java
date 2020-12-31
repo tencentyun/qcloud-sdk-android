11216a2758e809792628c60f8f5a0b4bc358afe1
jordanqin <jordanqin@tencent.com>
2020-12-31 18:13:09 +0800

    ut update

--- QCloudCosXml/cosxml/src/androidTestNormal/java/com/tencent/cos/xml/model/bucket/GetBucketVersionsTestAdapter.java
+++ QCloudCosXml/cosxml/src/androidTestNormal/java/com/tencent/cos/xml/model/bucket/GetBucketVersionsTestAdapter.java
@@ -12,7 +12,7 @@ public class GetBucketVersionsTestAdapter extends NormalRequestTestAdapter<ListB
     @Override
     protected ListBucketVersionsRequest newRequestInstance() {
         ListBucketVersionsRequest request = new ListBucketVersionsRequest(TestConst.PERSIST_BUCKET);
-        request.setPrefix("copy/");
+        request.setPrefix("do_not_remove_test/");
         request.setKeyMarker(null);
         request.setVersionIdMarker(null);
         request.setDelimiter("/");
