5fab5844fe3215429f6d5a847baf01654b26bf93
jordanqin <jordanqin@tencent.com>
2022-10-24 11:38:33 +0800

    update qcloud sdk to 5.9.6

--- QCloudCosXml/cos-android/src/normal/java/com/tencent/cos/xml/model/ci/asr/CreateSpeechJobsRequest.java
+++ QCloudCosXml/cos-android/src/normal/java/com/tencent/cos/xml/model/ci/asr/CreateSpeechJobsRequest.java
@@ -33,6 +33,7 @@ import com.tencent.cos.xml.exception.CosXmlClientException;
 import com.tencent.cos.xml.listener.CosXmlResultListener;
 import com.tencent.cos.xml.model.bucket.BucketRequest;
 import com.tencent.cos.xml.model.ci.asr.bean.CreateSpeechJobs;
+import com.tencent.cos.xml.model.tag.CallBackMqConfig;
 import com.tencent.qcloud.core.http.HttpConstants;
 import com.tencent.qcloud.core.http.RequestBodySerializer;
 import com.tencent.qcloud.qcloudxml.core.QCloudXml;
@@ -65,6 +66,13 @@ public class CreateSpeechJobsRequest extends BucketRequest {
      */
     public void setInputObject(@NonNull String object){
         createSpeechJobs.input.object = object;
+    }
+
+    /**
+     * 设置公网的Url，与Object必须有其中一个，且当两者都传入时，优先使用Object
+     */
+    public void setInputUrl(@NonNull String url){
+        createSpeechJobs.input.url = url;
     }
 
     /**
@@ -141,6 +149,119 @@ public class CreateSpeechJobsRequest extends BucketRequest {
         createSpeechJobs.operation.speechRecognition.convertNumMode = convertNumMode;
     }
 
+    /**
+     * 是否开启说话人分离：
+     * 0 表示不开启；
+     * 1 表示开启(仅支持8k_zh，16k_zh，16k_zh_video，单声道音频)。
+     * 默认值为 0。
+     * 注意：8k电话场景建议使用双声道来区分通话双方，设置ChannelNum=2即可，不用开启说话人分离。
+     */
+    public void setSpeakerDiarization(int speakerDiarization){
+        createSpeechJobs.operation.speechRecognition.speakerDiarization = speakerDiarization;
+    }
+
+    /**
+     * 仅支持非极速ASR
+     * 说话人分离人数（需配合开启说话人分离使用），取值范围：0-10。
+     * 0代表自动分离（目前仅支持≤6个人），1-10代表指定说话人数分离。默认值为 0。
+     */
+    public void setSpeakerNumber(int speakerNumber){
+        createSpeechJobs.operation.speechRecognition.speakerNumber = speakerNumber;
+    }
+
+    /**
+     * 是否过滤标点符号（目前支持中文普通话引擎）：
+     * 0 表示不过滤。
+     * 1 表示过滤句末标点。
+     * 2 表示过滤所有标点。
+     * 默认值为 0。
+     */
+    public void setFilterPunc(int filterPunc){
+        createSpeechJobs.operation.speechRecognition.filterPunc = filterPunc;
+    }
+
+    /**
+     * 输出文件类型，可选txt、srt。默认为txt
+     * 极速ASR仅支持txt
+     */
+    public void setOutputFileType(String outputFileType){
+        createSpeechJobs.operation.speechRecognition.outputFileType = outputFileType;
+    }
+
+    /**
+     * 是否开启极速ASR，可选true、false。默认为false
+     */
+    public void setFlashAsr(boolean flashAsr){
+        createSpeechJobs.operation.speechRecognition.flashAsr = flashAsr;
+    }
+
+    /**
+     * 极速ASR音频格式。支持 wav、pcm、ogg-opus、speex、silk、mp3、m4a、aac。
+     */
+    public void setFormat(String format){
+        createSpeechJobs.operation.speechRecognition.format = format;
+    }
+
+    /**
+     * 极速ASR参数。表示是否只识别首个声道，默认为1。0：识别所有声道；1：识别首个声道。
+     */
+    public void setFirstChannelOnly(int firstChannelOnly){
+        createSpeechJobs.operation.speechRecognition.firstChannelOnly = firstChannelOnly;
+    }
+
+    /**
+     * 极速ASR参数。表示是否显示词级别时间戳，默认为0。0：不显示；1：显示，不包含标点时间戳，2：显示，包含标点时间戳。
+     */
+    public void setWordInfo(int wordInfo){
+        createSpeechJobs.operation.speechRecognition.wordInfo = wordInfo;
+    }
+
+    /**
+     * 设置透传用户信息, 可打印的 ASCII 码, 长度不超过1024
+     */
+    public void setUserData(@NonNull String userData){
+        createSpeechJobs.operation.userData = userData;
+    }
+
+    /**
+     * 设置任务优先级，级别限制：0 、1 、2 。级别越大任务优先级越高，默认为0
+     */
+    public void setJobLevel(int jobLevel){
+        createSpeechJobs.operation.jobLevel = jobLevel;
+    }
+
+    /**
+     * 设置任务回调地址
+     * 优先级高于队列的回调地址。设置为 no 时，表示队列的回调地址不产生回调
+     */
+    public void setCallBack(@NonNull String callBack){
+        createSpeechJobs.callBack = callBack;
+    }
+
+    /**
+     * 设置任务回调格式，JSON 或 XML，默认 XML
+     * 优先级高于队列的回调格式
+     */
+    public void setCallBackFormat(@NonNull String callBackFormat){
+        createSpeechJobs.callBackFormat = callBackFormat;
+    }
+
+    /**
+     * 设置任务回调类型，Url 或 TDMQ，默认 Url
+     * 优先级高于队列的回调类型
+     */
+    public void setCallBackType(@NonNull String callBackType){
+        createSpeechJobs.callBackType = callBackType;
+    }
+
+    /**
+     * 设置任务回调TDMQ配置
+     * 当 CallBackType 为 TDMQ 时必填
+     */
+    public void setCallBackMqConfig(@NonNull CallBackMqConfig callBackMqConfig){
+        createSpeechJobs.callBackMqConfig = callBackMqConfig;
+    }
+
     @Override
     public String getPath(CosXmlServiceConfig cosXmlServiceConfig) {
         return "/asr_jobs";
@@ -161,7 +282,7 @@ public class CreateSpeechJobsRequest extends BucketRequest {
     @Override
     public void checkParameters() throws CosXmlClientException {
         super.checkParameters();
-        if(TextUtils.isEmpty(createSpeechJobs.input.object)){
+        if(TextUtils.isEmpty(createSpeechJobs.input.object) && TextUtils.isEmpty(createSpeechJobs.input.url)){
             throw new CosXmlClientException(ClientErrorCode.INVALID_ARGUMENT.getCode(), "input must be non-empty");
         }
 
