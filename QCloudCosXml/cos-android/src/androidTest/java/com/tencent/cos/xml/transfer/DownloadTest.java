221617ebcea5d88f729d9b7428533f3df741cb70
jordanqin <jordanqin@tencent.com>
2023-11-07 19:12:34 +0800

    update qcloud sdk to 5.9.20

--- QCloudCosXml/cos-android/src/androidTest/java/com/tencent/cos/xml/transfer/DownloadTest.java
+++ QCloudCosXml/cos-android/src/androidTest/java/com/tencent/cos/xml/transfer/DownloadTest.java
@@ -22,11 +22,9 @@
 
 package com.tencent.cos.xml.transfer;
 
-import static com.tencent.cos.xml.core.TestUtils.print;
 import static com.tencent.cos.xml.core.TestUtils.smallFilePath;
 
 import android.net.Uri;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -39,15 +37,12 @@ import com.tencent.cos.xml.core.TestLocker;
 import com.tencent.cos.xml.core.TestUtils;
 import com.tencent.cos.xml.exception.CosXmlClientException;
 import com.tencent.cos.xml.exception.CosXmlServiceException;
-import com.tencent.cos.xml.listener.CosXmlProgressListener;
 import com.tencent.cos.xml.listener.CosXmlResultListener;
 import com.tencent.cos.xml.model.CosXmlRequest;
 import com.tencent.cos.xml.model.CosXmlResult;
 import com.tencent.cos.xml.model.object.GetObjectRequest;
-import com.tencent.cos.xml.model.object.GetObjectResult;
 import com.tencent.qcloud.core.http.HttpTaskMetrics;
 import com.tencent.qcloud.core.logger.QCloudLogger;
-import com.tencent.qcloud.core.task.TaskManager;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -55,20 +50,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Dispatcher;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 @RunWith(AndroidJUnit4.class)
 public class DownloadTest {
@@ -105,6 +87,8 @@ public class DownloadTest {
         downloadTask.setCosXmlResultListener(new CosXmlResultListener() {
             @Override
             public void onSuccess(CosXmlRequest request, CosXmlResult result) {
+                File file = new File(getObjectRequest.getDownloadPath());
+                TestUtils.print("download file size:"+file.length());
                 testLocker.release();
             }
 
@@ -200,291 +184,6 @@ public class DownloadTest {
 
         testLocker.lock();
         TestUtils.assertCOSXMLTaskSuccess(downloadTask);
-    }
-
-    /**
-     * 批量文件下载
-     */
-    @Test public void testBatchSmallDownload() {
-        TransferManager transferManager = ServiceFactory.INSTANCE.newDefaultTransferManager();;
-        int count = 6;
-        final TestLocker testLocker = new TestLocker(count);
-        final AtomicInteger errorCount = new AtomicInteger(0);
-        final AtomicInteger successCount = new AtomicInteger(0);
-        final StringBuilder errorMessage = new StringBuilder();
-
-        for (int i = 0; i < count; i ++) {
-
-            final String localName = "1M_" + i + ".txt";
-            COSXMLDownloadTask downloadTask = transferManager.download(TestUtils.getContext(),
-                    TestConst.PERSIST_BUCKET, TestConst.PERSIST_BUCKET_BIG_OBJECT_PATH,
-                    TestUtils.localParentPath(), localName);
-
-            downloadTask.setCosXmlResultListener(new CosXmlResultListener() {
-                @Override
-                public void onSuccess(CosXmlRequest request, CosXmlResult result) {
-                    successCount.addAndGet(1);
-                    if(successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                    }
-                }
-
-                @Override
-                public void onFail(CosXmlRequest request, CosXmlClientException clientException, CosXmlServiceException serviceException) {
-                    TestUtils.printError(TestUtils.getCosExceptionMessage(clientException, serviceException));
-                    errorMessage.append(TestUtils.getCosExceptionMessage(clientException, serviceException)).append("/r/n");
-                    errorCount.addAndGet(1);
-                    if(successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                    }
-                }
-            });
-
-            downloadTask.setCosXmlProgressListener(new CosXmlProgressListener() {
-                @Override
-                public void onProgress(long complete, long target) {
-                    TestUtils.print(localName+ " download_Progress" + ": " + complete + "/" + target);
-                }
-            });
-            downloadTask.setTransferStateListener(new TransferStateListener() {
-                @Override
-                public void onStateChanged(TransferState state) {
-                    TestUtils.print(localName+ " download_State:" + state.name());
-                }
-            });
-        }
-
-        testLocker.lock();
-        TestUtils.assertErrorMessageNull(errorMessage);
-    }
-
-    /**
-     * 批量文件下载
-     */
-    @Test public void testBatchSmallGet() {
-        CosXmlSimpleService simpleService = ServiceFactory.INSTANCE.newAnonymousService();
-        int count = 36;
-        final TestLocker testLocker = new TestLocker(count);
-        final AtomicInteger errorCount = new AtomicInteger(0);
-        final AtomicInteger successCount = new AtomicInteger(0);
-
-        for (int i = 0; i < count; i ++) {
-            int finalI = i;
-            final String localName = "image_" + i + ".jpg";
-            GetObjectRequest request = new GetObjectRequest("ut-1257101689", String.format("small_images/%s.jpg", i+1),
-                    TestUtils.localParentPath() + File.separator + localName);
-            new Thread(() -> {
-                try {
-                    print("start_"+ finalI);
-                    long startTime = System.nanoTime();
-                    GetObjectResult result = simpleService.getObject(request);
-                    print("end_"+finalI);
-                    print("took_time_"+finalI+"_"+toSeconds(System.nanoTime() - startTime));
-                    successCount.addAndGet(1);
-                    if(successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                        testBatchSmallGet();
-                    }
-                } catch (CosXmlClientException e) {
-                    e.printStackTrace();
-                    errorCount.addAndGet(1);
-                    if(successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                        testBatchSmallGet();
-                    }
-                } catch (CosXmlServiceException e) {
-                    e.printStackTrace();
-                    errorCount.addAndGet(1);
-                    if(successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                        testBatchSmallGet();
-                    }
-                }
-            }).start();
-        }
-
-        testLocker.lock();
-    }
-
-    private double toSeconds(long nanotime) {
-        return (double)nanotime / 1_000_000_000.0;
-    }
-
-    /**
-     * 批量文件下载
-     */
-    @Test public void testBatchSmallGetNotCosSdk() {
-        int count = 36;
-        final TestLocker testLocker = new TestLocker(count);
-        final AtomicInteger errorCount = new AtomicInteger(0);
-        final AtomicInteger successCount = new AtomicInteger(0);
-
-        for (int i = 0; i < count; i ++) {
-            String urlString = String.format("https://mobile-ut-1253960454.cos.ap-guangzhou.myqcloud.com/small_images/%s.jpg", i+1);
-            int finalI = i;
-            new Thread(() -> {
-                HttpURLConnection getConnection = null;
-                try {
-                    URL url = new URL(urlString);
-                    print("start_"+ finalI);
-                    long startTime = System.nanoTime();
-                    getConnection = (HttpURLConnection) url.openConnection();
-                    getConnection.setRequestMethod("GET");
-
-                    int responseCode = getConnection.getResponseCode();
-                    print("end_"+finalI);
-                    print("took_time_"+finalI+"_"+toSeconds(System.nanoTime() - startTime));
-
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        successCount.addAndGet(1);
-                        if(successCount.get() + errorCount.get() == count) {
-                            testLocker.release();
-                        }
-                    } else {
-                        Log.e(TestConst.UT_TAG, "HTTP error code: " + responseCode);
-                        errorCount.addAndGet(1);
-                        if(successCount.get() + errorCount.get() == count) {
-                            testLocker.release();
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.e(TestConst.UT_TAG, "Error sending GET request: " + e.getMessage());
-                    errorCount.addAndGet(1);
-                    if(successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                    }
-                } finally {
-                    if (getConnection != null) {
-                        getConnection.disconnect();
-                    }
-                }
-            }).start();
-        }
-
-        testLocker.lock();
-    }
-
-    /**
-     * 批量文件下载
-     */
-    @Test public void testBatchSmallGetNotCosSdkOkhttp() {
-        int count = 36;
-        final TestLocker testLocker = new TestLocker(count);
-        final AtomicInteger errorCount = new AtomicInteger(0);
-        final AtomicInteger successCount = new AtomicInteger(0);
-
-        OkHttpClient client = new OkHttpClient();
-        Dispatcher dispatcher = client.dispatcher();
-        dispatcher.setMaxRequests(64); // 设置最大并发请求数
-        dispatcher.setMaxRequestsPerHost(36); // 设置每个主机的最大并发请求数
-
-        for (int i = 0; i < count; i ++) {
-            int finalI1 = i;
-            String urlString = String.format("https://mobile-ut-1253960454.cos.ap-guangzhou.myqcloud.com/small_images/%s.jpg", i + 1);
-            print("start_" + finalI1);
-            long startTime = System.nanoTime();
-            Request request = new Request.Builder()
-                    .url(urlString)
-                    .build();
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    e.printStackTrace();
-                    errorCount.addAndGet(1);
-                    if (successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                    }
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    print("end_" + finalI1);
-                    print("took_time_" + finalI1 + "_" + toSeconds(System.nanoTime() - startTime));
-                    successCount.addAndGet(1);
-                    if (successCount.get() + errorCount.get() == count) {
-                        testLocker.release();
-                    }
-                }
-            });
-        }
-        testLocker.lock();
-    }
-
-    /**
-     * 批量文件下载取消
-     */
-    @Test public void testBatchSmallDownloadCancel() {
-        TransferManager transferManager = ServiceFactory.INSTANCE.newDefaultTransferManager();;
-        int count = 36;
-        final TestLocker testLocker = new TestLocker(count);
-        List<COSXMLDownloadTask> taskList = new ArrayList<>();
-
-        TestUtils.print("第一批开始");
-        for (int i = 0; i < count; i ++) {
-            final String localName = "1_" + i + ".txt";
-            COSXMLDownloadTask downloadTask = transferManager.download(TestUtils.getContext(),
-                    TestConst.PERSIST_BUCKET, TestConst.PERSIST_BUCKET_SMALL_OBJECT_PATH,
-                    TestUtils.localParentPath(), localName);
-            taskList.add(downloadTask);
-
-            downloadTask.setCosXmlResultListener(new CosXmlResultListener() {
-                @Override
-                public void onSuccess(CosXmlRequest request, CosXmlResult result) {
-                    TestUtils.print("onSuccess1_"+((COSXMLDownloadTask.COSXMLDownloadTaskRequest)request).getDownloadPath());
-                }
-
-                @Override
-                public void onFail(CosXmlRequest request, CosXmlClientException clientException, CosXmlServiceException serviceException) {
-                    TestUtils.printError(TestUtils.getCosExceptionMessage(clientException, serviceException));
-                    TestUtils.print("onFail1_"+((COSXMLDownloadTask.COSXMLDownloadTaskRequest)request).getDownloadPath());
-                }
-            });
-            TestUtils.print("TaskPoolCount1_"+i+"_"+TaskManager.getInstance().getPoolCount());
-//            downloadTask.setCosXmlProgressListener(new CosXmlProgressListener() {
-//                @Override
-//                public void onProgress(long complete, long target) {
-//                    TestUtils.print(localName+ " download_Progress" + ": " + complete + "/" + target);
-//                }
-//            });
-//            downloadTask.setTransferStateListener(new TransferStateListener() {
-//                @Override
-//                public void onStateChanged(TransferState state) {
-//                    TestUtils.print(localName+ " download_State:" + state.name());
-//                }
-//            });
-        }
-
-        TestUtils.sleep(3000);
-        TestUtils.print("第一批取消");
-        for (COSXMLDownloadTask task : taskList){
-            task.cancel(true);
-        }
-
-        TestUtils.print("第二批开始");
-        for (int i = 100; i < 136; i ++) {
-            final String localName = "2_" + i + ".txt";
-            COSXMLDownloadTask downloadTask = transferManager.download(TestUtils.getContext(),
-                    TestConst.PERSIST_BUCKET, TestConst.PERSIST_BUCKET_SMALL_OBJECT_PATH,
-                    TestUtils.localParentPath(), localName);
-//            taskList.add(downloadTask);
-
-            downloadTask.setCosXmlResultListener(new CosXmlResultListener() {
-                @Override
-                public void onSuccess(CosXmlRequest request, CosXmlResult result) {
-                    TestUtils.print("onSuccess2_"+((COSXMLDownloadTask.COSXMLDownloadTaskRequest)request).getDownloadPath());
-                }
-
-                @Override
-                public void onFail(CosXmlRequest request, CosXmlClientException clientException, CosXmlServiceException serviceException) {
-                    TestUtils.printError(TestUtils.getCosExceptionMessage(clientException, serviceException));
-                    TestUtils.print("onFail2_"+((COSXMLDownloadTask.COSXMLDownloadTaskRequest)request).getDownloadPath());
-                }
-            });
-            TestUtils.print("TaskPoolCount2_"+i+"_"+TaskManager.getInstance().getPoolCount());
-        }
-
-        testLocker.lock();
     }
 
     /**
