9ebf1f14132338aa02df3c70d2a6ab31c9028286
jordanqin <jordanqin@tencent.com>
2024-01-05 20:25:33 +0800

    update qcloud sdk to 5.9.23

--- QCloudCosXml/cos-android/src/androidTest/java/com/tencent/cos/xml/MyApplication.java
+++ QCloudCosXml/cos-android/src/androidTest/java/com/tencent/cos/xml/MyApplication.java
@@ -2,8 +2,6 @@ package com.tencent.cos.xml;
 
 import android.app.Application;
 
-import com.tencent.qcloud.track.BuildConfig;
-
 /**
  * <p>
  * Created by jordanqin on 2023/9/14 21:38.
@@ -17,10 +15,10 @@ public class MyApplication extends Application {
     }
 
     private void trackInit(){
-        CosTrackService.initCLs(this,
-                "9ab664b4-f657-4ef9-8b8c-305b819e477d",
-                "ap-guangzhou.cls.tencentcs.com",
-                BuildConfig.CLS_SECRET_ID,
-                BuildConfig.CLS_SECRET_KEY);
+//        CosTrackService.initCLs(this,
+//                "9ab664b4-f657-4ef9-8b8c-305b819e477d",
+//                "ap-guangzhou.cls.tencentcs.com",
+//                BuildConfig.CLS_SECRET_ID,
+//                BuildConfig.CLS_SECRET_KEY);
     }
 }
